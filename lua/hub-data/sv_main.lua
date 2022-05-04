eb581b16334bcebf49b4a527839b14572cad7641
fesh <40101968+qfork@users.noreply.github.com>
2022-05-04 14:27:51 -0700

    Added recursive function to get server IP

--- lua/hub-data/sv_main.lua
+++ lua/hub-data/sv_main.lua
@@ -8,7 +8,15 @@ end
 
 hub_data.conn:connect()
 
-do
+// game.GetIPAddress() will return 0.0.0.0:port when called too early, so we loop till it's available
+function hub_data.waitForIPAddress()
+	if (game.GetIPAddress():find('0.0.0.0')) then
+		return timer.Simple(0, function() 
+			// error occurs when indexing with hub_data.waitForIPAddress 🧐
+			hub_data['waitForIPAddress']() 
+		end)
+	end
+
 	local query = hub_data.conn:query(("REPLACE INTO `ServerInfo` VALUES ('%s', '%s', '%s', %s, %s)"):format(game.GetIPAddress(), GetConVar('hostname'):GetString(), game.GetMap(), player.GetCount(), game.MaxPlayers()))
 
 	query.onError = function(self, err, sql)
@@ -18,29 +26,25 @@ do
 	query:start()
 end
 
+hook('Initialize', 'hub-data', hub_data.waitForIPAddress)
+
 local preparedError = function(self, err)
 	print('query error: ', err)
 end
 
-local updatePlayerCount = hub_data.conn:prepare("UPDATE `ServerInfo` SET `players` = ?")
+local updatePlayerCount = hub_data.conn:prepare("UPDATE `ServerInfo` SET `players` = ? WHERE `ip` = ?")
 updatePlayerCount.onError = preparedError
 
 hook.Add("PlayerInitialSpawn", "hub-data", function(pl)
 	updatePlayerCount:setNumber(1, player.GetCount())
+	updatePlayerCount:setString(2, game.GetIPAddress())
 	updatePlayerCount:start()
 end)
 
 hook.Add("PlayerDisconnected", "hub-data", function(pl)
 	timer.Simple(0, function()
 		updatePlayerCount:setNumber(1, player.GetCount())
+		updatePlayerCount:setString(2, game.GetIPAddress())
 		updatePlayerCount:start()
 	end)
-end)
-
-local updateHostname = hub_data.conn:prepare("UPDATE `ServerInfo` SET `hostname` = ?")
-updateHostname.onError = preparedError
-
-cvars.AddChangeCallback('hostname', function(convar, old, new)
-	updateHostname:setString(1, new)
-	updateHostname:start()
 end)
